8a56a5fb562a5b072e8dd26881494c9b04eb721d
teclir <raimondotecla@gmail.com>
2024-08-20 15:01:45 +0200

    completed ejercicio switch case

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,85 @@
+﻿int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+//Ejercicio: Realización de una actividad de desafío con instrucciones switch//
+
+// SKU = Stock Keeping Unit
+string sku = "02-GN-S";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Hoodie";
+        break;
+    case "02":
+        type = "Jacket";
+        break;
+    case "03":
+        type = "Shorts";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Blue";
+        break;
+    case "GN":
+        color = "Green";
+        break;
+    case "RD":
+        color = "Red";
+        break;
+    default:
+        color = "Gray";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
